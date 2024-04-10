c4124c16fbac67e3a707d6a69b25267d70da1c34
DanielTran97 <tranbinhnguyen97@gmail.com>
2024-04-10 22:23:23 +0800

    base WIP version

---
+++ data_port.py
@@ -0,0 +1,53 @@
+import sqlite3
+from webstock_scraper import *
+
+conn = sqlite3.connect('Stock_performance.db')
+c = conn.cursor()
+
+# Database already created
+
+c.execute('''CREATE TABLE IF NOT EXISTS Stock_Performance(
+          ID INTEGER PRIMARY KEY,
+          Symbol TEXT,
+          Name TEXT, 
+          Price_Intraday DECIMAL, 
+          Change DECIMAL, 
+          Percent_Change DECIMAL(5,4), 
+          Market_Cap  TEXT, 
+          Vol_in_Curr_0000_UTC TEXT,
+          Vol_in_Curr_24Hr TEXT,
+          Total_vol_Curr_24Hr TEXT,
+          Circ_Supply TEXT,
+          DateTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP)
+          ''')
+
+for data in crypto_scrape_data:
+    c.execute('''INSERT INTO Stock_Performance (Symbol, Name, Price_Intraday, Change, Percent_Change, Market_Cap, Vol_in_Curr_0000_UTC, Vol_in_Curr_24Hr, Total_vol_Curr_24Hr, Circ_Supply, DateTime) 
+              VALUES(?,?,?,?,?,?,?,?,?,?,?)''',(data['item_selector'], 
+                                                data['name_selector'], 
+                                                data['price_selector'], 
+                                                data['change_selector'],
+                                                data['changePercentage_selector'],
+                                                data['marketCap_selector'],
+                                                data['Vol_InCurrencyUTC_selector'],
+                                                data['Vol_InCurrencyHour_selector'],
+                                                data['TotalVol_InCurrencyHour_selector'],
+                                                data['Circul_Supply_selector'],
+                                                data['DateTime']))
+
+# conn.commit()
+
+# conn.close()
+
+c.execute('''SELECT * FROM Stock_Performance''')
+results = c.fetchall()
+print(results)
+
+c.execute('''DELETE FROM Stock_Performance''')
+
+# STEP 1: FILTER DATA BY CRYPTO NAME (SQLITE)
+
+# STEP 2: FORMULATE PERFORMANCE GRAPH OF DATA'S CAPTURED WEEKLY BASED OFF STEP 1 (MACHINE LEARNING)
+
+# STEP 3: PERFORM DATA ANALYSIS ON EACH CRYPTO'S DATA BASED OFF WEEKLY PERFORMANCES (MACHINE LEARNING)
+
---
+++ webstock_scraper.py
@@ -0,0 +1,42 @@
+import requests
+import datetime
+from bs4 import BeautifulSoup
+
+# For Cryptocurrencies
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
+}
+
+# paste stock web url from Yahoo Finance
+url = 'https://finance.yahoo.com/crypto/'
+
+r = requests.get(url,headers= headers)
+
+soup = BeautifulSoup(r.content, 'lxml')
+
+# simpTblRow
+crypto_scrape_data = []
+
+for item in soup.select('.simpTblRow'):
+    scrape_dict = {}
+    scrape_dict['item_selector'] = item.select('[aria-label=Symbol]')[0].get_text()
+    scrape_dict['name_selector'] = item.select('[aria-label=Name]')[0].get_text()
+    scrape_dict['price_selector'] = item.select('[aria-label="Price (Intraday)"]')[0].get_text()
+    scrape_dict['change_selector'] = item.select('[aria-label=Change]')[0].get_text()
+    scrape_dict['changePercentage_selector'] = item.select('[aria-label="% Change"]')[0].get_text()
+    scrape_dict['marketCap_selector'] = item.select('[aria-label="Market Cap"]')[0].get_text()
+    scrape_dict['Vol_InCurrencyUTC_selector'] = item.select('[aria-label="Volume in Currency (Since 0:00 UTC)"]')[0].get_text()
+    scrape_dict['Vol_InCurrencyHour_selector'] = item.select('[aria-label="Volume in Currency (24Hr)"]')[0].get_text()
+    scrape_dict['TotalVol_InCurrencyHour_selector'] = item.select('[aria-label="Total Volume All Currencies (24Hr)"]')[0].get_text()
+    scrape_dict['Circul_Supply_selector'] = item.select('[aria-label="Circulating Supply"]')[0].get_text()
+    scrape_dict['DateTime'] = (datetime.datetime.now()).strftime("%Y-%m-%d %H:%M:%S")
+
+    crypto_scrape_data.append(scrape_dict)
+
+# for data in crypto_scrape_data:
+#     print(data)
+
+#     print()
+#     print('-----------Divider----------')
+#     print()
